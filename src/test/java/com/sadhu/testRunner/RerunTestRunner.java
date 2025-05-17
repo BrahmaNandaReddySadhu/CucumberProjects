a5a8f78da1fc5bf7a287db418a09ebc77c1a8475
Brahmananda Reddy Sadhu <sadhubrahmanandareddy@gmail.com>
2025-05-17 21:58:10 +0530

    adding rerun plugin

--- src/test/java/com/sadhu/testRunner/RerunTestRunner.java
+++ src/test/java/com/sadhu/testRunner/RerunTestRunner.java
@@ -1,12 +1,12 @@
 package com.sadhu.testRunner;
 
-
+import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.*;
 
 @CucumberOptions(
-        features = "classpath:features",
+        features = "target/failedrerun.txt",
         glue = {"com.sadhu.stepDef", "com.sadhu.hooks", "com.sadhu.listeners"},
         plugin = {"pretty",
                 "html:target/cucumber-html-report.html",
@@ -18,15 +18,8 @@ import org.testng.annotations.*;
                 "rerun:target/failedrerun.txt"
         },
         monochrome = true
-
 )
-public class Runner extends AbstractTestNGCucumberTests {
 
-    @Override
-    @DataProvider(parallel = false)
-    public Object[][] scenarios() {
-        return super.scenarios();
-    }
-
+public class RerunTestRunner extends AbstractTestNGCucumberTests{
 
 }
